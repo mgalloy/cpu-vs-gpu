b29b2486399d13eab0e6c281ffff150cf278d961
Jon Petter Åsen <jonpetter@sqhead.com>
2014-02-11 13:53:55 +0100

    Moved legends to positions originally used in IDL script.

--- cpu_vs_gpu_plot.py
+++ cpu_vs_gpu_plot.py
@@ -24,9 +24,10 @@ def makePlot():
     dataTicks = ["%s"%v for v in range(2000,2015,2)]
     dataTicksValues = pl.datestr2num(["%s-01-01"%v for v in range(2000,2015,2)])
 
-    #bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
-        
-    pl.figure()
+    #bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)   
+    
+    fig = pl.figure()
+    pl.subplot(121)
     for d in data.values():
         x = pl.datestr2num(d['dates'])
         y = d['flops']
@@ -35,15 +36,20 @@ def makePlot():
         for n in range(len(d['names'])):
             pl.text(x[n]-50, y[n]+50, d['names'][n], 
                     ha="right", va="top", size=10, color=c)#, bbox=bbox_props)
+        pl.text(x[-1]+50, y[-1], d['legend'], 
+                ha="left", va="center", size=12, color=c)
 
-    pl.legend(loc='upper left')
+    #pl.legend(loc='upper left')
     pl.xlabel('Release date')
     pl.ylabel('Theoretical peak (GFLOP/s)')
     pl.xlim((pl.datestr2num('2000-01-01'), pl.datestr2num('2014-01-01')))
     pl.xticks(dataTicksValues, dataTicks)
     pl.ylim((0, 5000))
+    fig.subplots_adjust(right=1.8)
     #pl.gca().set_yscale('log')
     pl.show()
+    
+    #fig.savefig('cpu_vs_gpu', bbox_extra_artists=(txt,), bbox_inches='tight')
 
 
 if __name__ == "__main__":
