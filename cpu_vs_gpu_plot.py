f0584ae45588b38f961dc5a8c3b209d8e49386f3
Jon Petter Åsen <jonpetter@sqhead.com>
2014-02-12 10:17:35 +0100

    Added SP data for Geforce GTX 780 Ti. Revised plot function.

--- cpu_vs_gpu_plot.py
+++ cpu_vs_gpu_plot.py
@@ -24,33 +24,36 @@ def makePlot():
     dataTicks = ["%s"%v for v in range(2000,2015,2)]
     dataTicksValues = pl.datestr2num(["%s-01-01"%v for v in range(2000,2015,2)])
 
-    #bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)   
+    bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9) 
+    textSize = 10
     
     fig = pl.figure()
-    pl.subplot(121)
+    #pl.subplot(121)
     for d in data.values():
         x = pl.datestr2num(d['dates'])
         y = d['flops']
         c = d['color']
         pl.plot(x, y, 'o-', color=c, label=d['legend'], lw=2)
-        for n in range(len(d['names'])):
-            pl.text(x[n]-50, y[n]+50, d['names'][n], 
-                    ha="right", va="top", size=10, color=c)#, bbox=bbox_props)
-        pl.text(x[-1]+50, y[-1], d['legend'], 
-                ha="left", va="center", size=12, color=c)
+        if d['legend'] is not 'Intel CPU SP':
+            for n in range(len(d['names'])):
+                pl.text(x[n]-50, y[n]+50, d['names'][n], 
+                        ha="right", va="top", size=textSize, color=c)#, bbox=bbox_props)
+        pl.text(x[-1]+150, y[-1], d['legend'], 
+                ha="left", va="center", size=textSize+2, color=c,bbox=bbox_props)
 
     #pl.legend(loc='upper left')
-    pl.xlabel('Release date')
-    pl.ylabel('Theoretical peak (GFLOP/s)')
+    pl.xlabel('Release date', fontsize=textSize+4)
+    pl.ylabel('Theoretical peak (GFLOP/s)', fontsize=textSize+4)
     pl.xlim((pl.datestr2num('2000-01-01'), pl.datestr2num('2014-01-01')))
-    pl.xticks(dataTicksValues, dataTicks)
-    pl.ylim((0, 5000))
-    fig.subplots_adjust(right=1.8)
+    pl.xticks(dataTicksValues, dataTicks, fontsize=textSize)
+    pl.yticks(fontsize=textSize)
+    pl.ylim((0, 5500))
+    #fig.subplots_adjust(right=1.8)
     #pl.gca().set_yscale('log')
     pl.show()
     
-    #fig.savefig('cpu_vs_gpu', bbox_extra_artists=(txt,), bbox_inches='tight')
-
+    fig.savefig('cpu_vs_gpu.png', bbox_inches='tight')
+    fig.savefig('cpu_vs_gpu.pdf', bbox_inches='tight')
 
 if __name__ == "__main__":
     makePlot()
